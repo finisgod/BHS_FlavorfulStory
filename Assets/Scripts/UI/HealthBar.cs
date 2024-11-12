d12f132b260afee90961dedf2c07b31e2155edd7
ArMaGeDoN <ilyakur025@gmail.com>
2024-11-12 22:45:56 +0300

    Переезд

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,35 +1,67 @@
-﻿using TMPro;
+using FlavorfulStory.Stats.CharacterStats;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
-/// <summary> Класс-стартер сцены для верного порядка инициализаций (на будущее).</summary>
-public class HealthBar : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+
+namespace FlavorfulStory.UI
 {
-    [SerializeField]TMP_Text textObject;
-    [SerializeField] Player player;
-    Image HealthImage;
-    void Start()
+    /// <summary>  Класс, отвечающий за отрисовку количества здоровья.</summary>
+    public class HealthBar : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        SetHealth(player.Health);
-        player.OnChangeHealth += OnHealthChanged;
-        textObject.gameObject.SetActive(false);
-    }
-    void SetHealth(int health)
-    {
-        textObject.text = health.ToString();
-    }
-    private void OnHealthChanged(int health)
-    {
-        SetHealth(health);
-    }
+        /// <summary> Объект текста.</summary>
+        [SerializeField] private TMP_Text _textObject;
+        
+        /// <summary> Объект игрока.</summary>
+        [SerializeField] private GameObject _player;
+        
+        /// <summary> Компонент здоровья.</summary>
+        private Health _health;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        textObject.gameObject.SetActive(true);
-    }
+        /// <summary> Подписка на события.</summary>
+        private void OnEnable()
+        {
+            _health.OnHealthChanged += SetHealthText;
+        }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        textObject.gameObject.SetActive(false);
+        /// <summary> Отписка от события.</summary>
+        private void OnDisable()
+        {
+            _health.OnHealthChanged -= SetHealthText;
+        }
+
+        private void Awake()
+        {
+            _health = _player.GetComponent<Health>();
+        }
+
+        private void Start()
+        {
+            SetHealthText(_health.CurrentHealth);
+            _textObject.gameObject.SetActive(false);
+        }
+        
+        /// <summary> Установка текстового значения.</summary>
+        /// <param name="health"> Текущее значение энергии.</param>
+        private void SetHealthText(int health)
+        {
+            _textObject.text = health.ToString();
+        }
+        
+
+        /// <summary> Включение объекта при наведении на него курсора.</summary>
+        /// <param name="eventData"> Информация ивента.</param>
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            SetHealthText(_health.CurrentHealth);
+            _textObject.gameObject.SetActive(true);
+        }
+
+        
+        /// <summary> Выключение объекта при наведении на него курсора.</summary>
+        /// <param name="eventData"> Информация ивента.</param>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _textObject.gameObject.SetActive(false);
+        }
     }
 }
